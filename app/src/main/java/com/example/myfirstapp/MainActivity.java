1d9810d02e1715b29f6ce40674ea42b693859fd8
W0476462 <w0476462@nscc.ca>
2023-09-11 16:35:40 -0300

    2023-09-11 Notes for the assignment

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myfirstapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
+
+        binding.txtWelcomeMsg.setText(R.string.welcome_message);
+
+        // Logging
+        Log.i("TESTING", "Hello Log!!");
+
+        // Dynamically add views to layout
+        LinearLayout linearLayout = binding.linearLayout;
+
+        for (int i = 0; i < 20; i++) {
+            TextView textView = new TextView(this);
+            textView.setText("TextView :" + String.valueOf(i));
+            linearLayout.addView(textView);
+
+        }
+
+        // Handle Button Click Event
+        binding.buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("TESTING", "Button Submit Clicked");
+                Log.i("TESTING", "Name: " + binding.editTextName.getText().toString());
+
+                String message = "Welcome " + binding.editTextName.getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
